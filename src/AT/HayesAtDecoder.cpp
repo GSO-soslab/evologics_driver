c9fde67bf714aedb48d9d246382336fa701c0109
Jason <jason_miller@uri.edu>
2024-10-22 19:30:41 +0000

    compare bytes in vector instead of elements in vector

--- src/AT/HayesAtDecoder.cpp
+++ src/AT/HayesAtDecoder.cpp
@@ -42,6 +42,7 @@ void AtDecoder::decode(const std::string &raw)
     //the data is from the ':' to the end
     std::string data = strip.substr(second_colon_index + 1);
 
+    int vector_size = 0;
     //if the data contains a comma delimeter then split and push back
     size_t comma_index = data.find_first_of(',');
     if(comma_index != std::string::npos)
@@ -53,6 +54,7 @@ void AtDecoder::decode(const std::string &raw)
             std::string substr;
             getline(ss, substr, ',');
             msg.data.push_back(substr);
+            vector_size += 1;
 
         }
     }
@@ -62,68 +64,21 @@ void AtDecoder::decode(const std::string &raw)
         msg.data.push_back(data);
     }
 
-    // std::string::size_type read_position = raw.find_first_of('?');
-    // std::string::size_type write_position = raw.find_first_of('!');
 
-    // std::string split_str = "+++AT:";
-    // size_t split_index = raw.find(split_str);
-
-    // std::string strip_str = raw.substr(split_index + split_str.size());
-
-    // size_t colon_index = strip_str.find_first_of(':');
-    // std::string data_str;
-
-    // if(read_position != std::string::npos || write_position != std::string::npos)
-    // {
-
-    //     msg.command = strip_str.substr(0,colon_index);
-
-    //     strip_str = strip_str.substr(colon_index+1);
-
-    //     colon_index = strip_str.find_first_of(':');
-
-    //     data_str = strip_str.substr(colon_index+1);
-    // }
-    // else
-    // {
-    //     size_t comma_index = strip_str.find_first_of(',', colon_index+1);
-    //     size_t read = comma_index - colon_index - 1;
-    //     msg.command = strip_str.substr(colon_index+1, read);
-
-    //     data_str = strip_str.substr(comma_index);
-    // }
-
-    // size_t delimeter_index = data_str.find_first_of(',');
-
-    // if(delimeter_index == std::string::npos)
-    // {
-    //     msg.data.push_back(data_str);
-    // }
-    // else
-    // {
-    //     while (delimeter_index != std::string::npos)
-    //     {
-    //         // Delimeter is pointing to the comma at the start of this field.
-
-    //         // Find the index of the next comma.
-    //         size_t next_delimiter_index = data_str.find_first_of(',', delimeter_index + 1);
-
-    //         // Following comma found, set length using position of next comma.
-    //         size_t read_length = next_delimiter_index - delimeter_index - 1;
-
-    //         // Pull substring into field.
-    //         msg.data.push_back(data_str.substr(delimeter_index + 1, read_length));
-
-    //         // Update delimeter index.
-    //         delimeter_index = next_delimiter_index;
-    //     }
-    // }
+    for(auto i: msg.data)
+    {
+        vector_size += i.size();
+    }
 
     if(cb_)
     {
-        if((size_t)size == msg.data.size())
+        if(size == vector_size)
         {
             cb_(msg);   
+        }
+        else
+        {
+            printf("PARSED SIZE DOES NOT MATCH ACTUAL SIZE\n");
         }
 
     }
